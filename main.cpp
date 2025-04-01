3d75801e9a789c9f0825151f44ae6553f02f31b4
Edwin Setsoafia <edwinsetsoafia9@gmail.com>
2025-04-01 10:48:12 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,6 @@
   
     #include "mechatron.h"
 
-    #ifdef __cplusplus
-    extern "C" {
-    #endif
-    
-    #include "System Configration.h"
-    #include "stm32f0xx_ll_system.h"
-    
-    #ifdef __cplusplus
-    }
-    #endif
-
 
     int main(){
       //set up code
@@ -22,8 +11,8 @@
   
       while(1){
             ON_BOARD_LED.write(0); // Turns on board LED off
-            ThisThread::sleep_for(100000); // delays for unknown number of seconds
+            ThisThread::sleep_for(1000000); // delays for unknown number of seconds
             ON_BOARD_LED.write(1); // Turns on board LED on
-            hisThread::sleep_for(100000); // delays for unknown number of seconds
+            hisThread::sleep_for(1000000); // delays for unknown number of seconds
         }
     }
